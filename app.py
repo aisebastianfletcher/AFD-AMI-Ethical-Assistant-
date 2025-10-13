1fb99b164e37ec25e1f45b3515f129e4ef761af0
aisebastianfletcher <aisebastianfletcher@gmail.com>
2025-10-13 07:01:20 -0100

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import os
-from afd_ami_core import AFDInfinityAMI
+from afd_ami_core.py import AFDInfinityAMI
 import pandas as pd
 import matplotlib.pyplot as plt
 import time
